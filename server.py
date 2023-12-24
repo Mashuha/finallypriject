0da160c1a0eb99db3ead2265a216ae09fec2960e
Glitch (frequent-spectacled-charger) <none>
2023-12-24 12:50:57 +0000

    👭🚪 Checkpoint ./server.py:63672580/4524 ./templates/home.html:63672580/2157

--- server.py
+++ server.py
@@ -11,11 +11,25 @@ from matplotlib import pyplot as plt
 app = Flask(__name__)
 
 soup = BeautifulSoup(requests.get("https://www.pogoda.msk.ru/").content, 'html.parser')
-
+table = soup.findAll('table')[6]
+status = table.find("tr").find('th', {"id":"dt"})
+data = {}
+for row in table.findAll("tr"):
+  if row.has_attr('class'):
+   if status.text != row.find('th', {"id":"dt"}).text:
+      status = row.find('th', {"id":"dt"})
+  if row.has_attr('valign'):
+    if status.text[:5] not in data:
+      data[status.text[:5]] = []
+    row_data = row.findAll('td')
+    data[status.text[:5]].append(dict(
+        temp=list(map(int, row_data[2].text.split('..'))),
+        davl=int(row_data[3].text.strip('%'))
+    ))
 
 @app.route("/", methods=['POST', 'GET'])
 def main_page():
-    return render_template('home.html')
+    return render_template('home.html', dates = list(data))
 
 
 @app.route("/aboutus", methods=['POST', 'GET'])
@@ -25,18 +39,37 @@ def aboutus():
 
 @app.route("/result", methods=['POST'])
 def result():
-    city = request.form['start_date']
-    return city
-    # ax.set_xticks(keys)
-    # ax.set_xticklabels(keys, rotation=-45, fontsize=6)
-    # ax.set_ylabel('Кол-во часов')
-    # ax.set_xlabel('Преподаватели' if orderBy == Tibox.OrderBy.TEACHERS else 'Даты')
-    # ax.grid()
-    # buf = BytesIO()
-    # fig.savefig(buf, format='png')
-    # data = base64.b64encode(buf.getbuffer()).decode('ascii')
+    dt = request.form['start_date']
+    
+    max_list = {}
+    full_temp = {}
+    for day in list(data):
+      for block in data[day]:
+        if day not in max_list:
+          max_list[day] = []
+        if day not in full_list:
+          full_list[day] = []
+        max_list[day].append(max(block['temp']))
+        for i in block['temp']:
+          full_list[day].append(i)
 
-    return render_template("result.html")
+    temp_max_list = []
+    keys = []
+    for day in list(max_list):
+      for i, temp in enumerate(max_list[day]):
+        temp_max_list.append(temp)
+        keys.append(day + f'({i+1})')
+    
+    fig, ax = plt.subplots()
+    ax.set_xticklabels(keys, rotation=-45)
+    ax.set_ylabel('max temp')
+    ax.set_xlabel('Дни(<периоды>)')
+    ax.grid()
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    data = base64.b64encode(buf.getbuffer()).decode('ascii')
+
+    return render_template("result.html",image = data, max=max(full_temp[dt]), min=min(ull_temp[dt]), avg=full_temp[dt]/len(full_temp[dt]))
 
 
 if __name__ == '__main__':
